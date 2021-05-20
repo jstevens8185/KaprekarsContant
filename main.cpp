a2cf18df48ab9aa7c90618ba535c74a8242144ce
Jered Stevens <j.stevens8185@gmail.com>
2021-05-20 15:12:55 -0500

    init

--- main.cpp
+++ main.cpp
@@ -14,14 +14,40 @@
 // Write a function that returns how many steps this will take for a given input N.
 
 #include <iostream>
-#include "jstand.cpp"
+#include <string>
 
 using namespace std;
 
 
 
 int main(){
+//Step 0: Declare contant as array
+//          KapConst = 6174
 
+//step 1:   Get Starting Value as string
+
+//Step 2:   Verify it is a four digit number
+//  Step 2.1:   if string size is 4 continue
+//              else throw error and restart
+
+//Step 3:   Flip Number
+//  Step 3.1:   Convert String to int
+//  Step 3.2:   Convert int to array
+//  Step 3.3:   Copy array backwards
+
+//Step 4:   Decide witch is bigger
+
+//Step 5:   Subtract smaller number from bigger number
+
+//Step 6:   Result becomes new starting value
+
+//Step 7:   Increment count
+
+//Step 8:   Check if number is equal to 6174
+
+//Step 9:   Repeat until number is equal to 6174
+
+//Step 10:  Print count
 
 
 return 0;
